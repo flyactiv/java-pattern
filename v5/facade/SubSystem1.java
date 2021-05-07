b64a2a71ba9b6d8fb1ebe4bcb7fd5d6380b6ff77
Alexandra <70800450+flyactiv@users.noreply.github.com>
2021-05-07 11:45:44 +0300

    Delete v5 directory

--- v5/facade/SubSystem1.java
+++
@@ -1,7 +0,0 @@
-package pr7.v5.facade;
-
-public class SubSystem1 {
-    public void start() {
-        System.out.println("Subsystem1 start working");
-    }
-}
--- v5/facade/SubSystem2.java
+++
@@ -1,7 +0,0 @@
-package pr7.v5.facade;
-
-public class SubSystem2 {
-    public void start() {
-        System.out.println("Subsystem2 start working");
-    }
-}
